dee99f29229d5800d5dcbeff9959da6973938a72
Lev Kozlov <kozlov.l.a10@gmail.com>
2023-11-03 07:55:14 +0300

    chore: cleanup

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM quay.io/pypa/manylinux_2_28_x86_64:latest
-
-# install wget
-RUN yum install -y wget
--- scripts/before_build.sh
+++
@@ -1,9 +0,0 @@
-# check if yum is package manager
-if [ -f /usr/bin/yum ]; then
-    yum install -y wget
-fi
-
-if [ -f /usr/bin/apt-get ]; then
-    apt-get update
-    apt-get install -y wget
-fi
